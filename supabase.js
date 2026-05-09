7a1aff65cfa7d6156bbf1f1108593b882d118436
CashCan Pro <jlshop06190@gmail.com>
2026-05-09 12:01:35 +0100

    Update supabase client initialization method

--- supabase.js
+++ supabase.js
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://vbdgumnlpgpiaeckqnum.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyZXd6Z2VtenFtb2tpbmx5bGh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMzgyNzQsImV4cCI6MjA5MzgxNDI3NH0.vBh00PCILcjrcGynLto-5Ce7zfRvjTXMUDqzG1PWGMw'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+window.supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
