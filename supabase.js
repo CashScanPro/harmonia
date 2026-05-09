f1ecb7ade22e7fdfcba5954ffe3a8a3ae9d31c70
CashCan Pro <jlshop06190@gmail.com>
2026-05-09 11:20:51 +0100

    Replace Supabase key with new JWT token
    Updated the Supabase key to a new JWT token.

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://vbdgumnlpgpiaeckqnum.supabase.co'
-const supabaseKey = 'sb_publishable_XhMaZnPLA3-lk8fjzkBalw_4JUaTmV1'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyZXd6Z2VtenFtb2tpbmx5bGh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyMzgyNzQsImV4cCI6MjA5MzgxNDI3NH0.vBh00PCILcjrcGynLto-5Ce7zfRvjTXMUDqzG1PWGMw'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
